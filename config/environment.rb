1226a83de38aa2929a6b1754bbc22b4545da3a50
Vladislav Sychev <vladislav.sychev@gmail.com>
2013-05-13 18:55:01 +0400

    ru month contracts

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Tz::Application.initialize!
 
-Date::DATE_FORMATS[:ru_date] = "%d.%m.%Y"
+Date::DATE_FORMATS[:ru_date] = "%d.%M.%Y"
