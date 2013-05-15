dfdecf8ccba9a0efb5b09d077e5e7fd98d7ee994
Vladislav Sychev <vladislav.sychev@gmail.com>
2013-05-16 00:51:40 +0400

    change letter to taxiziru@ya

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,7 +6,7 @@ class UserMailer < ActionMailer::Base
    @user = user
    @url = "http://taxizi.ru/signin"
    email_with_name = "#{@user.name} <#{@user.email}>"
-   mail(:to => email_with_name, :bcc => "user@taxizi.ru", :subject => "Taxi Zi регистрация #{@user.name} на сайте прошла успешна")
+   mail(:to => email_with_name, :bcc => "taxiziru@yandex.ru", :subject => "Taxi Zi регистрация #{@user.name} на сайте прошла успешна")
   end
 
   def update_email(user)
@@ -20,39 +20,39 @@ class UserMailer < ActionMailer::Base
     @contract = contract
     @url = "http://taxizi.ru/contracts/#{@contract.id}"
     email_with_name = "#{@contract.contractor_name} <#{@contract.contractor_email}>"
-    mail(to: email_with_name, bcc: "contract@taxizi.ru", subject: "Подтвердите свой заказ #{@contract.t_car} на Taxi Zi")
+    mail(to: email_with_name, bcc: "taxiziru@yandex.ru", subject: "Подтвердите свой заказ #{@contract.t_car} на Taxi Zi")
   end
 
   def new_offer_email(offer)
     @offer = offer
     @url = "http://taxizi.ru/offers/#{@offer.id}"
     email_with_name = "#{@offer.contract.contractor_name} <#{@offer.contract.contractor_email}>"
-    mail(to: email_with_name, bcc: "offer@taxizi.ru", subject: "Ваш заказ на Taxi Zi получил новое предложение")
+    mail(to: email_with_name, bcc: "taxiziru@yandex.ru", subject: "Ваш заказ на Taxi Zi получил новое предложение")
   end
 
   def offer_taken_email(offer)
     @offer = offer
     email_with_name2 = "#{@offer.contract.contractor_name} <#{@offer.contract.contractor_email}>"
     email_with_name = "#{@offer.user.name} <#{@offer.user.email}>"
-    mail(to: email_with_name, cc: email_with_name2, bcc: "deal@taxizi.ru", subject: "Предложение на заказ на сайте Taxi Zi принято")
+    mail(to: email_with_name, cc: email_with_name2, bcc: "taxiziru@yandex.ru", subject: "Предложение на заказ на сайте Taxi Zi принято")
   end
 
   def newpass_email(user)
     @user = user
     email_with_name = "#{@user.name} <#{@user.email}>"
-    mail(to: email_with_name, bcc: "user@taxizi.ru", subject: "Новый пароль #{@user.name} на сайте Taxi Zi.")
+    mail(to: email_with_name, bcc: "taxiziru@yandex.ru", subject: "Новый пароль #{@user.name} на сайте Taxi Zi.")
   end
 
   def posts_email(user)
     @user = user
-    mail(to: "help@taxizi.ru", subject: "Post from Contact or Help page")
+    mail(to: "taxiziru@yandex.ru", subject: "Post from Contact or Help page")
   end
 
   def infoletter_email(contract)
     @contract = contract
     @uawe_all = Array.new
     @user_all = User.select(:email).where(:name => 'VLADISLAV SYCHEV').map(&:email)
-    mail(to: "contract@taxizi.ru", bcc: @user_all, subject: "Открытый заказ #{@contract.t_car} на Taxi Zi")
+    mail(to: "taxiziru@yandex.ru", bcc: @user_all, subject: "Открытый заказ #{@contract.t_car} на Taxi Zi")
   end
 
 end
