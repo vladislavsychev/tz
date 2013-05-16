bf41da7578cb455f29c5aa4bebf3e9feae5db4a5
Vladislav Sychev <vladislav.sychev@gmail.com>
2013-05-16 22:43:40 +0400

    dropbox image

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'bootstrap-will_paginate', '0.0.6'
 gem 'jquery-rails', '2.0.2'
 gem 'paperclip', "~> 3.0"
 gem 'rails-i18n'
+gem 'paperclip-dropbox'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
--- app/models/attached_asset.rb
+++ app/models/attached_asset.rb
@@ -18,7 +18,16 @@ class AttachedAsset < ActiveRecord::Base
   attr_accessible :asset, :asset_file_name
 
   belongs_to :attachable, :polymorphic => true
-  has_attached_file :asset, :styles => { :mini => "240x120#", :middle => "360x180#", :normal => "600x300#" }
+  has_attached_file :asset, 
+                    :storage => :dropbox,
+                    :dropbox_credentials => "#{Rails.root}/config/dropbox.yml",
+                    :styles => { 
+                    :mini => "240x120#", 
+                    :middle => "360x180#", 
+                    :normal => "600x300#" },
+                    :dropbox_options => {
+                    :path => proc { |style| "#{style}/#{id}_#{asset.original_filename}" }
+                     }
 
   validates_attachment   :asset, :content_type => { :content_type => /^image\/(png|jpg|jpeg)/}, :size => { :in => 0..200.kilobytes }
 end
